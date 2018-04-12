3e8ef5781fffb607787209bdacfbe9b2d6360b53
Alex Yan <alexyan1024@gmail.com>
2018-04-12 01:46:14 -0500

    SpringMVCConfig

---
+++ src/main/java/my/spring/Spring_02_MVC_Config/App.java
@@ -0,0 +1,13 @@
+package my.spring.Spring_02_MVC_Config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
